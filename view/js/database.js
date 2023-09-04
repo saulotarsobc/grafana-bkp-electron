ba1e5de9849dfc7b95bd5c46d1871ad322caae14
Saulo Costa <saulotarsobc@gmail.com>
2023-09-04 12:08:10 -0300

    :fire:

--- view/js/database.js
+++ view/js/database.js
@@ -2,3 +2,5 @@ const dbFile = join(remote.app.getPath("userData"), "dbFile.sqlite"); //console.
 const sequelize = new Sequelize({ dialect: "sqlite", storage: dbFile });
 const Servers = sequelize.define("servers", { name: { type: DataTypes.STRING }, address: { type: DataTypes.STRING }, token: { type: DataTypes.STRING } });
 Servers.sync({ alter: true });
+
+console.log(dbFile);
